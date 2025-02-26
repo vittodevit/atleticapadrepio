2edf6987a81edd33d47d4e8dd33bc07c5a3bcaa0
Vittorio Lo Mele <hi@vitto.dev>
2025-02-26 16:32:14 +0100

    tentativi CI 2

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias.canvas = false
+    config.resolve.alias.encoding = false
+    return config
+  }
 };
 
 export default nextConfig;
